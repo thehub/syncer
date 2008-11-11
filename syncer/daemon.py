3aeb4b1b386b9af0830cadd40dfe47a99d787db7
shekhar <shekhar@9b48f8db-4976-470e-bc89-e604267d2a29>
2008-11-11 12:05:38 +0000

    events

--- syncer/daemon.py
+++ syncer/daemon.py
@@ -30,10 +30,13 @@ ldapwriter.ignore_old_failures = True
 transactionmgr = subscribers.trmgr.TransactionMgr("transactionmgr")
 transactionmgr.ignore_old_failures = True
 
+# Transactions
 syncer.completeTransactions.addSubscriber(transactionmgr)
 syncer.completeTransactions.transactional = False
 syncer.rollbackTransactions.addSubscriber(transactionmgr)
 syncer.rollbackTransactions.transactional = False
+
+# Signon/off
 syncer.onSignon.addSubscriber(sessionkeeper)
 syncer.onSignon.addSubscriber(ldapwriter)
 syncer.onSignon.addSubscriber(hubspace)
@@ -48,6 +51,12 @@ syncer.onSignoff.addSubscriber(sessionkeeper)
 syncer.onSignoff.transactional = False
 syncer.onSignoff.join = True
 
+# User/Group events
+syncer.onUserAdd.addSubscriber(sessionkeeper)
+#syncer.onUserAdd.addSubscriber(hubspace)
+syncer.onUserAdd.addSubscriber(ldapwriter)
+syncer.onUserAdd.join = True
+
 syncer.onUserMod.addSubscriber(sessionkeeper)
 syncer.onUserMod.addSubscriber(hubspace)
 syncer.onUserMod.addSubscriber(ldapwriter)
@@ -57,28 +66,33 @@ syncer.onAssignRoles.addSubscriber(hubspace)
 syncer.onAssignRoles.addSubscriber(ldapwriter)
 syncer.onAssignRoles.join = True
 
-syncer.onUserAdd.addSubscriber(sessionkeeper)
-#syncer.onUserAdd.addSubscriber(hubspace)
-syncer.onUserAdd.addSubscriber(ldapwriter)
-syncer.onUserAdd.join = True
-
-syncer.onHubAdd.addSubscriber(sessionkeeper)
-syncer.onHubAdd.addSubscriber(hubspace)
-syncer.onHubAdd.addSubscriber(ldapwriter)
-
-syncer.onHubMod.addSubscriber(sessionkeeper)
-syncer.onHubMod.addSubscriber(hubspace)
-syncer.onHubMod.addSubscriber(ldapwriter)
-
 syncer.onRoleAdd.addSubscriber(sessionkeeper)
 syncer.onRoleAdd.addSubscriber(hubspace)
 syncer.onRoleAdd.addSubscriber(ldapwriter)
 
+# Hub events
+syncer.onHubAdd.addSubscriber(sessionkeeper)
+syncer.onHubAdd.addSubscriber(hubspace)
+syncer.onHubAdd.addSubscriber(ldapwriter)
+syncer.onHubMod.addSubscriber(sessionkeeper)
+syncer.onHubMod.addSubscriber(hubspace)
+syncer.onHubMod.addSubscriber(ldapwriter)
+
+# AccessPolicy events
 syncer.onAccesspolicyAdd.addSubscriber(sessionkeeper)
 syncer.onAccesspolicyAdd.addSubscriber(ldapwriter)
+syncer.onAccesspolicyMod.addSubscriber(sessionkeeper)
+syncer.onAccesspolicyMod.addSubscriber(ldapwriter)
+syncer.onAccesspolicyDel.addSubscriber(sessionkeeper)
+syncer.onAccesspolicyDel.addSubscriber(ldapwriter)
 
+# OpenTimes events
 syncer.onOpentimesAdd.addSubscriber(sessionkeeper)
 syncer.onOpentimesAdd.addSubscriber(ldapwriter)
+syncer.onOpentimesMod.addSubscriber(sessionkeeper)
+syncer.onOpentimesMod.addSubscriber(ldapwriter)
+syncer.onOpentimesDel.addSubscriber(sessionkeeper)
+syncer.onOpentimesDel.addSubscriber(ldapwriter)
 
 Pyro.core.initServer()
 #Pyro.config.PYRO_TRACELEVEL=3
