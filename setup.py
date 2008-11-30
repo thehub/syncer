ef720b88581f7a68ec4629f5762ba46d1a54a9e1
shekhar <shekhar@9b48f8db-4976-470e-bc89-e604267d2a29>
2008-11-30 10:06:51 +0000

    Explicitly specifying URL

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ setup(name='syncer',
       version='0.2',
       packages=find_packages(),
       install_requires=[
-           "pyro",
+           "Pyro",
            "Elixir",
+      ],
+      dependency_links = [
+            "http://downloads.sourceforge.net/pyro/Pyro-3.8.tar.gz?modtime=1219451921&big_mirror=0"
       ]
       )
