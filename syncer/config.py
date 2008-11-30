b8781f0589e169f7ed1bc75b7ce88225bad0b9c0
shekhar <shekhar@9b48f8db-4976-470e-bc89-e604267d2a29>
2008-11-30 14:41:28 +0000

    Simplifying syncer configuration

--- syncer/config.py
+++ syncer/config.py
@@ -2,9 +2,15 @@ import os, datetime, Pyro
 
 use_ssl = False
 pyrobasedir = os.path.dirname(os.path.abspath(__file__))
-pyroconf_path = "%s/Pyro.conf" % pyrobasedir
-Pyro.config.setup(pyroconf_path)
+#pyroconf_path = "%s/Pyro.conf" % pyrobasedir
+#Pyro.config.setup(pyroconf_path)
+Pyro.config.PYRO_HOST = "ldap.the-hub.net"
+Pyro.config.PYRO_PORT = 9003
+Pyro.config.PYRO_STDLOGGING = 0
+Pyro.config.PYRO_LOGFILE = "daemon.log"
+Pyro.config.PYRO_TRACELEVEL = 1
 Pyro.config.PYROSSL_CERTDIR = "%s/certs" % pyrobasedir
+
 syncer_path = "sync"
 if use_ssl:
     syncer_uri =  "PYROLOCSSL://%s:%s/%s" % (Pyro.config.PYRO_HOST, Pyro.config.PYRO_PORT, syncer_path)
@@ -19,12 +25,12 @@ logdir = os.path.join(syncerroot, "logs")
 user_agent = "syncer"
 subscriber_adminemail = ""
 
-ldap_uri = "ldap://localhost"
+ldap_uri = "ldap://ldap.the-hub.net"
 ldap_basedn = "dc=the-hub,dc=net"
 
 session_idletimeout = datetime.timedelta(0, 60 * 24 * 60 * 60) # 60 days of inactivity should expire the session
 
-client_disabled = True
+client_disabled = False
 
 class defaults(object):
     event_join = False
