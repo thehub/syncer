5f041cc00a89c672dfe0e626956f1fb0317facb7
shekhar <shekhar@9b48f8db-4976-470e-bc89-e604267d2a29>
2008-12-19 05:22:16 +0000

    add twill which appears to be a dependency

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='syncer',
       install_requires=[
            "Pyro",
            "Elixir",
+           "twill"
       ],
       dependency_links = [
             "http://downloads.sourceforge.net/pyro/Pyro-3.8.tar.gz?modtime=1219451921&big_mirror=0"
