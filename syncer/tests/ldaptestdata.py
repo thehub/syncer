7430b595f552f549cb2ef1e4b610b0b5f4e4bb77
shekhar <shekhar@9b48f8db-4976-470e-bc89-e604267d2a29>
2009-01-12 15:20:03 +0000

    ldapwriter tests

---
+++ syncer/tests/ldaptestdata.py
@@ -0,0 +1,79 @@
+import decimal
+import datetime
+
+class TestData():
+    """
+    """
+
+root_u = "ldapadmin"
+root_p = "cL5XgIxJK0"
+host_u = "x"
+host_p = "x"
+hub1 = TestData()
+hub1.user1 = TestData()
+
+hub1.id = 1
+hub1.data = [('telephoneNumber', u'0123 456 0000'),
+                ('billingVATID', u'456 1111 11'),
+                ('bankAccountNumber', u'11111177'),
+                ('labeledURI', u'http://members.the-hub.net'),
+                ('bankIBAN', u'ABCD CPBK 7777 7777 777777'),
+                ('bankName', u'The Bank'),
+                ('bankSwiftCode', u'CCCCCC22'),
+                ('bankSortCode', u'999299'),
+                ('currency', u'GBP'),
+                ('hubId', hub1.id),
+                ('billingAddress', u'5 Some Street, London'),
+                ('billingCompanyNumber', u'5111115'),
+                ('timezone', u'Europe/London'),
+                ('ou', u'Islington'),
+                ('hubImageMimetype', u'image/png'),
+                ('billingPaymentTerms', u'15 days')]
+
+hub1.user1.uid = "shon"
+hub1.user1.p = "secret"
+hub1.user1.data = [('billingTelephoneNumber', None),
+                ('cn', u'Shekhar Tiwatne'),
+                ('uid', u'shon'),
+                ('publicViewable', None),
+                ('operatingSystem', u'Linux'),
+                ('gn', u'Shekhar'),
+                ('homeHub', 'hubId=1,ou=hubs,o=the-hub.net'),
+                ('quotaStorage', None),
+                ('billingOutstanding', decimal.Decimal("0.00")),
+                ('title', u'Mr'),
+                ('facsimileTelephoneNumber', None),
+                ('hubImageMimetype', u'image/png'),
+                ('organisation', u'Open Coop'),
+                ('hubUserReference', u'uid=shon,ou=users,o=the-hub.net'),
+                ('hubIdentitySIPURI', None),
+                ('sambaLMPassword', u'7B5F83F76983185A7584248B8D2C9F9E'),
+                ('mail', u'shekhar.tiwatne@the-hub.net'),
+                ('postalAddress', u'Pune'),
+                ('userPassword', '{MD5}ca0dusoBt4aH1v5iA0kJHg=='),
+                ('billingCompany', u''),
+                ('extensionTelephoneNumber', None),
+                ('homeTelephoneNumber', u'+91 20 12345'),
+                ('description',
+                 u'Just a programmer\n'),
+                ('storageLocation', None),
+                ('dateCreated', datetime.datetime(2009, 1, 1, 18, 17, 20)),
+                ('billingReminderCounter', 0),
+                ('billingAddress', u'Tower Bridge'),
+                ('active', 1),
+                ('hubWelcomeSent', 1),
+                ('sambaNTPassword', u'46EBAD5FF06F009D8E3A8B7995E38B9D'),
+                ('skypeId', u'shon___'),
+                ('telephoneNumber', u'None'),
+                ('billingVATID', u''),
+                ('hubUserId', 1),
+                ('displayName', u'Shekhar Tiwatne'),
+                ('labeledURI', None),
+                ('mobile', '+91 99222 55300'),
+                ('billingProfile', 1),
+                ('mailAlso', [u'shon@thisdoesnotexists.net']),
+                ('billingFacsimileNumber', None),
+                ('sn', u'Tiwatne'),
+                ('billingMail', None),
+                ('billingReminderKey', u'66cbf1d00bc76b9a34f82388f5c6b85f')]
+hub1.user1.moddata = [('title', 'Mr.')]
---
+++ syncer/tests/ldaptester.py
@@ -0,0 +1,143 @@
+# LDAP Tests
+# * ACL tests
+import sys
+import unittest
+import logging
+import logging.handlers
+
+import syncer
+import syncer.client
+import syncer.config
+import syncer.errors
+syncer.config.host = "localhost"
+syncer.config.ldap_uri = "ldap://localhost"
+syncer.config.reload()
+
+import ldaptestdata as testdata
+
+class SyncerConn(object):
+    def __init__(self, u):
+        self.username = u
+        self.session = dict ()
+        session_getter = lambda : self.session
+        self.clnt = syncer.client.SyncerClient("LDAPTest", session_getter)
+
+trs_list = []
+
+def setUpModule():
+    print "setting up.. "
+
+class SyncerTestCase(unittest.TestCase):
+    short_dscr = ""
+    def __call__(self):
+        try:
+            ret = self._run()
+            logger.info(self.__class__.__name__)
+            return ret
+        except Exception, err:
+            msg = "(%s) %s" % (self.__class__.__name__, str(err))
+            logger.error(msg)
+            #raise
+            sys.exit(0)
+
+class SignOn(SyncerTestCase):
+    e_ret = True
+    def __init__(self, u, p):
+        self.u = u
+        self.p = p
+    def _run(self):
+        conn = SyncerConn(self.u)
+        ret = conn.clnt.onSignon(self.u, self.p)
+        tr_id, res = ret
+        if not conn.clnt.isSuccessful(res):
+            msg = syncer.errors.res2errstr(res, ", ")
+            self.fail(msg)
+        conn.clnt.setSyncerToken(res['sessionkeeper']['result'])
+        return conn
+
+class AddHub(SyncerTestCase):
+    e_ret = False
+    def __init__(self, conn, data):
+        self.conn = conn
+        self.data = data
+    def _run(self):
+        tr_id, res = self.conn.clnt.onHubAdd(self.data.id, self.data.data)
+        if not self.conn.clnt.isSuccessful(res):
+            self.fail(syncer.errors.res2errstr(res, ", "))
+
+
+class AddHubAsHost(AddHub):
+    e_ret = True
+
+class TearDownModule():
+    pass
+
+def setupLogging():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s %(levelname)-8s: line %(lineno)s %(message)s', datefmt="%Y.%m.%d %H:%M:%S")
+
+    logger.setLevel(logging.DEBUG)
+    console = logging.StreamHandler()
+    console.setFormatter(formatter)
+    console.setLevel(logging.INFO)
+    logger.addHandler(console)
+
+    flog = logging.handlers.RotatingFileHandler("syncertests.log", 'a', 1024 * 1024, 10)
+    flog.setLevel(logging.DEBUG)
+    flog.setFormatter(formatter)
+    logger.addHandler(flog)
+    globals()["logger"] = logger
+
+class SignOnAsRoot(SignOn):
+    pass
+
+class SignOnAsUser1(SignOn):
+    pass
+
+class AddUser(SyncerTestCase):
+    def __init__(self, conn, data):
+        self.conn = conn
+        self.data = data
+    def _run(self):
+        tr_id, res = self.conn.clnt.onUserAdd(self.data.uid, self.data.data)
+        if not self.conn.clnt.isSuccessful(res):
+            self.fail(syncer.errors.res2errstr(res))
+
+class AddUser1AsRoot(AddUser):
+    pass
+
+class SyncerConns(object):
+    """I do nothing"""
+
+class ModUser(SyncerTestCase):
+    def __init__(self, conn, data):
+        self.conn = conn
+        self.data = data
+    def _run(self):
+        tr_id, res = self.conn.clnt.onUserMod(self.data.uid, self.data.moddata)
+        if not self.conn.clnt.isSuccessful(res):
+            self.fail(syncer.errors.res2errstr(res))
+
+class ModUserAsMember(ModUser):
+    pass
+
+
+def main():
+    setupLogging()
+    conns = SyncerConns()
+    signOnAsRoot = SignOnAsRoot(testdata.root_u, testdata.root_p)
+    conns.root_conn = signOnAsRoot()
+    addHub1AsRoot = AddHub(conns.root_conn, testdata.hub1)
+    addHub1AsRoot()
+    addUser1AsRoot = AddUser1AsRoot(conns.root_conn, testdata.hub1.user1)
+    addUser1AsRoot()
+    signOnAsUser1 = SignOnAsUser1(testdata.hub1.user1.uid, testdata.hub1.user1.p)
+    conns.user1_conn = signOnAsUser1()
+    modUserAsMember = ModUserAsMember(conns.user1_conn, testdata.hub1.user1)
+    modUserAsMember()
+
+
+if __name__ == '__main__':
+    main()
+
